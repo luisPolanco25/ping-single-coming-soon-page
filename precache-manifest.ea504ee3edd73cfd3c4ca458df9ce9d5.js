5499eb7252dcef42724a79e2a0e3f32188048326
LuisP <luisjavierpolancomorel@gmail.com>
2021-05-30 18:44:21 -0400

    Updates

---
+++ precache-manifest.ea504ee3edd73cfd3c4ca458df9ce9d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894462c11cd951137ba6ee35e2bfdd3c",
+    "url": "/ping-single-coming-soon-page/index.html"
+  },
+  {
+    "revision": "b88e1c4daecfaf87d643",
+    "url": "/ping-single-coming-soon-page/static/css/main.5792a24f.chunk.css"
+  },
+  {
+    "revision": "d64428e2a19b901ddba4",
+    "url": "/ping-single-coming-soon-page/static/js/2.c36114de.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/ping-single-coming-soon-page/static/js/2.c36114de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b88e1c4daecfaf87d643",
+    "url": "/ping-single-coming-soon-page/static/js/main.a549bb24.chunk.js"
+  },
+  {
+    "revision": "176b6c94875132572ca4",
+    "url": "/ping-single-coming-soon-page/static/js/runtime-main.4ab6e0ca.js"
+  },
+  {
+    "revision": "92dd5c8fe5e8dde945a6f01abca7e7cc",
+    "url": "/ping-single-coming-soon-page/static/media/illustration-dashboard.92dd5c8f.png"
+  },
+  {
+    "revision": "e392d534418b8f8c25ecb73731147704",
+    "url": "/ping-single-coming-soon-page/static/media/logo.e392d534.svg"
+  }
+]);
