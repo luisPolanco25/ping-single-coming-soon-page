5131b09fbf0b4116b2efbdec97b36461d163c4ec
LuisP <luisjavierpolancomorel@gmail.com>
2021-05-31 11:16:44 -0400

    Title attribute added to social media buttons

--- src/components/page/Page.js
+++ src/components/page/Page.js
@@ -16,9 +16,9 @@ export const Page = () => {
             <img src={dashboard} alt="Dashboard" id="dashboard" />
 
             <div id="social-media">
-                <button aria-label="Facebook button"><i className="fab fa-facebook-f"></i></button>
-                <button aria-label="Twitter button"><i className="fab fa-twitter"></i></button>
-                <button aria-label="Instagram button"><i className="fab fa-instagram"></i></button>
+                <button title="Facebook" aria-label="Facebook button"><i className="fab fa-facebook-f"></i></button>
+                <button title="Twitter" aria-label="Twitter button"><i className="fab fa-twitter"></i></button>
+                <button title="Instagram" aria-label="Instagram button"><i className="fab fa-instagram"></i></button>
             </div>
             <p>&copy; Copyright Ping. All rights reserved.</p>
         </main>
